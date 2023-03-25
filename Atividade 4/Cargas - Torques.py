d10cf11fb6865f22acfc0778d9a530375e2ef8f4
Paulo Andrade <paulo.andrade@alunos.ufersa.edu>
2023-03-25 14:18:45 -0300

    Correção dos cálculos (falta conferir incoerências)

--- Atividade 4/Cargas - Torques.py
+++ Atividade 4/Cargas - Torques.py
@@ -4,84 +4,77 @@ import json
 with open('./inputs.json', 'r') as f: 
     dados = json.load(f)
 
-
 # -------------------------------------- Torque do Aileron ----------------------------------------------------
 
 # Área do Aileron
-baseA = dados["aileron"]["base_aileron"]
-alturaA = dados["aileron"]["altura_aileron"]
-area_aileron =  baseA * alturaA
+area_aileron = dados["aileron"]["base_aileron"] * dados["aileron"]["altura_aileron"]
 
 # Centróide
-centroide_base = baseA / 2
-centroide_altura = alturaA / 2
+centroide_base = dados["aileron"]["base_aileron"] / 2
+centroide_altura = dados["aileron"]["altura_aileron"] / 2
 
-# Cálculo da Força ()
+# Cálculo da Força
 forca = (dados["aileron"]["n_maximo"] * dados["aileron"]["w_aileron"] * area_aileron) / dados["aileron"]["area_asa"]
 
+# Corda no ponto do centróide
+corda_centroide = dados["aileron"]["altura_aileron"] * 100
+
 # Cálculo da Distância do Braço do retângulo
-distancia_retangulo = centroide_altura
+# TODO: conferir se a divisão é por 2 ou por 3
+distancia_retangulo = corda_centroide / 2
 
 # Cálculo do Torque do Aileron (T = F * d) / 2 - Biplano
-
 torqueA = (forca * distancia_retangulo) / 2
 
-# Opcional:
-print ("Área do Aileron: ", area_aileron, "Centímetros ao Quadrado")
-print ("Valores do Centróide - Base e Altura: ", centroide_base, " e ", centroide_altura )
-print ("Torque do Aileron Biplano: ", torqueA, "KgFCm")
-
-
+# Output:
+print("Distância: ", distancia_retangulo, "cm")
+print("Força: ", forca, "kgf")
+print("Torque: ", torqueA, "kgf.cm")
 
 # -------------------------------------- Torque do Profundor ----------------------------------------------------
 
-corda_eh = dados["profundor"]["corda_empenagem_horizontal"]
-p_total = dados["profundor"]["maior_carga_eh"]
-ce_profundor = dados ["profundor"]["corda_profundor"]
-baseP = dados ["profundor"]["base_profundor"]
-alturaP = dados ["profundor"]["altura_profundor"]
-
 # Área do Profundor
-area_profundor = baseP * alturaP
+print()
 
 # Carregamento de pressão na maior corda da eh em kgf (W)
-w_empenagem_horizontal = (2 * p_total) / corda_eh
+w_empenagem_horizontal = (2 * dados["profundor"]["maior_carga_eh"]) \
+                         / dados["profundor"]["corda_empenagem_horizontal"] / 9.81
 
 # Carregamento de pressão na maior corda do profundor (W')
-w_profundor = (w_empenagem_horizontal * ce_profundor) / corda_eh
+w_profundor = (w_empenagem_horizontal * dados["profundor"]["corda_profundor"]) \
+              / dados["profundor"]["corda_empenagem_horizontal"]
 
 # Força do Profundor
-forca_profundor = (ce_profundor * w_profundor) / 2
+forca_profundor = (dados["profundor"]["corda_profundor"] * w_profundor) / 2
 
 # Distância do Braço
-distancia_bracoP = alturaP / 3
+# TODO: conferir se é a altura ou a corda do profundor
+distancia_bracoP = dados["profundor"]["altura_profundor"] * 100 / 3
 
-# Torque do Profundor (T = F * d) 
+# Torque do Profundor (T = F * d)
 torqueP = forca_profundor * distancia_bracoP
 
-print ()
-print ("Área do Profundor: ", area_profundor, "Centímetros ao Quadrado")
-print ("Força e Distância do Braço do Profundor: ", forca_profundor, "e", distancia_bracoP)
-print ("Torque do Profundor: ", torqueP, "KgFCm")
-
-
-
+print("Distância: ", distancia_bracoP, "cm")
+print("Força: ", forca_profundor, "kgf")
+print("Torque do Profundor: ", torqueP, "kgf.cm")
 
 # --------------------------------------- Torque do Leme ----------------------------------------------------
 
-baseL = dados ["leme"]["base_leme"]
-alturaL = dados ["leme"]["altura_leme"]
-
-# Área 
+print()
 # Proporção de Área entre Empenagem Vertical e Leme
 area = dados["leme"]["area_leme"] / dados["leme"]["area_empenagem_vertical"]
+print(area)
 
 # Cálculo da Força do Leme
-forca_leme = area * dados["leme"]["carga_ev_rajada"]
+# TODO: conferir divisão pela gravidade
+forca_leme = area * dados["leme"]["carga_ev_rajada"] / 9.81
+
+# Distância do braço
+distancia_bracoL = dados["leme"]["altura_leme"] * 100 / 2
 
 # Torque do Leme
-torqueL = forca_leme * (alturaL / 2)
+torqueL = forca_leme * distancia_bracoL
 
-
-print ()
-print ("Torque do Leme: ", torqueL, "KgFCm")
+print("Distância: ", distancia_bracoL, "cm")
+print("Força: ", forca_leme, "kgf")
+print("Torque do Leme: ", torqueL, "kgf.cm")
