f7ff4ee8e1071be35ef620824d2ca1ec7cb493b6
Paulo Andrade <paulo.andrade@alunos.ufersa.edu>
2023-04-03 17:14:19 -0300

    Adição da variação do Hcg e da função para exibição

--- Atividade 5/Código - Estabilidade.py
+++ Atividade 5/Código - Estabilidade.py
@@ -5,16 +5,16 @@ with open('./inputs.json', 'r') as f:
     dados = json.load(f)
 
 
-def contribuicao_asa(modelo):
+def contribuicao_asa(modelo, posicao_cg_corda_media):
     # Coeficiente angular da curva de momento de arfagem gerado pela asa
     coef_ang_curva_momento_arf = dados[modelo]["C_angular_curva_CLxAlfa"] \
-                                 * (dados[modelo]["posicao_CG_corda_media"]
+                                 * (posicao_cg_corda_media
                                     - dados[modelo]["posicao_centro_aerod_corda_media"])
 
     # Coeficiente de momento de arfagem gerado pela asa para o ângulo de ataque nulo
     coef_momento_arf_ang_0 = dados[modelo]["C_momento_redor_centro"] \
                              + dados[modelo]["C_sustentacao_angulo_ataque_nulo"] \
-                             * (dados[modelo]["posicao_CG_corda_media"]
+                             * (posicao_cg_corda_media
                                 - dados[modelo]["posicao_centro_aerod_corda_media"])
 
     return {"coef_ang_curva_momento_arf": coef_ang_curva_momento_arf,
@@ -63,80 +63,78 @@ def contribuicao_fuselagem(modelo):
             "coef_momento_arf_ang_0": coef_momento_arf_ang_0}
 
 
-def contribuicao_total(asa, empenagem_horizontal, fuselagem):
+def contribuicao_total(cont_asa, cont_empenagem_horizontal, cont_fuselagem):
     # Coeficiente angular da curva de momento de arfagem total
-    coef_ang_curva_momento_arf = asa["coef_ang_curva_momento_arf"] \
-                                 + empenagem_horizontal["coef_ang_curva_momento_arf"] \
-                                 + fuselagem["coef_ang_curva_momento_arf"]
+    coef_ang_curva_momento_arf = cont_asa["coef_ang_curva_momento_arf"] \
+                                 + cont_empenagem_horizontal["coef_ang_curva_momento_arf"] \
+                                 + cont_fuselagem["coef_ang_curva_momento_arf"]
 
     # Coeficiente de momento de arfagem para o ângulo de ataque nulo total
-    coef_momento_arf_ang_0 = asa["coef_momento_arf_ang_0"] \
-                                 + empenagem_horizontal["coef_momento_arf_ang_0"] \
-                                 + fuselagem["coef_momento_arf_ang_0"]
+    coef_momento_arf_ang_0 = cont_asa["coef_momento_arf_ang_0"] \
+                             + cont_empenagem_horizontal["coef_momento_arf_ang_0"] \
+                             + cont_fuselagem["coef_momento_arf_ang_0"]
 
     return {"coef_ang_curva_momento_arf": coef_ang_curva_momento_arf, "coef_momento_arf_ang_0": coef_momento_arf_ang_0}
 
 
 def teste_estabilidade(total):
-    if total["coef_ang_curva_momento_arf"] < 0 and total["coef_momento_arf_ang_0"] > 0:
-        estabilidade = True
-    else:
-        estabilidade = False
-
-    return estabilidade
+    return total["coef_ang_curva_momento_arf"] < 0 and total["coef_momento_arf_ang_0"] > 0
 
 
-def ponto_neutro(empenagem_horizontal, fuselagem, modelo_asa):
+def ponto_neutro(cont_empenagem_horizontal, cont_fuselagem, modelo_asa):
     return (dados[modelo_asa]["posicao_centro_aerod_corda_media"]
-           - (fuselagem["coef_ang_curva_momento_arf"] / dados[modelo_asa]["C_angular_curva_CLxAlfa"])
-           - (empenagem_horizontal["coef_ang_curva_momento_arf"] / dados[modelo_asa]["C_angular_curva_CLxAlfa"])) * 100
+            - (cont_fuselagem["coef_ang_curva_momento_arf"] / dados[modelo_asa]["C_angular_curva_CLxAlfa"])
+            - (cont_empenagem_horizontal["coef_ang_curva_momento_arf"]
+               / dados[modelo_asa]["C_angular_curva_CLxAlfa"])) * 100
 
 
-def margem_estatica(modelo_asa, ponto_neutro):
-    return ((ponto_neutro / 100) - dados[modelo_asa]["posicao_CG_corda_media"]) * 100
+def margem_estatica(modelo_asa, ponto_neutro, hcg):
+    # Quanto maior a Margem Estatística, maior será a estabilidade
+    # 10% < Margem Estatística < 20%  ->  Boa Estabilidade e Manobrabilidade
+    return ponto_neutro - (hcg * 100)
+
+
+def calcular_estabilidade(modelo_asa, modelo_empenagem_horizontal, modelo_fuselagem, hcg_inicial, hcg_final):
+    # Contribuições
+    cont_emp_horizontal = contribuicao_empenagem_horizontal(modelo_empenagem_horizontal)
+    cont_fuselagem = contribuicao_fuselagem(modelo_fuselagem)
+
+    # Inicialização de variáveis
+    hcg = hcg_inicial
+    resultados = []
+
+    # Variação do Hcg
+    while hcg <= hcg_final+0.001:
+        cont_asa = contribuicao_asa(modelo_asa, hcg)
+        cont_total = contribuicao_total(cont_asa, cont_emp_horizontal, cont_fuselagem)
+
+        # Teste de estabilidade
+        estabilidade = teste_estabilidade(cont_total)
+        if estabilidade:
+            # Ponto neutro e Margem Estática
+            pnt_neutro = ponto_neutro(cont_emp_horizontal, cont_fuselagem, modelo_asa)
+            mrgm_estatica = margem_estatica(modelo_asa, pnt_neutro, hcg)
+
+            resultados.append({"hcg": hcg, "contribuicao_asa": cont_asa,
+                               "contribuicao_emp_horizontal": cont_emp_horizontal,
+                               "contribuicao_fuselagem": cont_fuselagem, "ponto_neutro": pnt_neutro,
+                               "margem_estatica": mrgm_estatica, "estabilidade": estabilidade})
+
+        # Aumento do Hcg
+        hcg += 0.01
+
+    return resultados
+
+
+def exibir_resultados(resultados):
+    for i in resultados:
+        for j in i:
+            if j == "hcg":
+                print(f"\33[92mHcg - {i[j]} \33[m")
+            else:
+                print(f"{j} - {i[j]}")
+        print()
+
 
 # -=-=- OUTPUT -=-=-
-
-print()
-
-print("\33[92m---------- Contribuição da Asa \33[m")
-asa = contribuicao_asa("asa_2022")
-print("Coeficiente Angular da Curva de momento de Arfagem : ", asa["coef_ang_curva_momento_arf"])
-print("Coeficiente de momento de Arfagem para o Ângulo de Ataque Nulo : ", asa["coef_momento_arf_ang_0"])
-
-print()
-
-emp_horizontal = contribuicao_empenagem_horizontal("empenagem_horizontal_2022")
-print("\33[92m---------- Contribuição da Empenagem Horizontal \33[m")
-print("Coeficiente Angular da Curva de Momento de Arfagem : ", emp_horizontal["coef_ang_curva_momento_arf"])
-print("Coeficiente de Momento de Arfagem para o Ângulo de Ataque Nulo : ", emp_horizontal["coef_momento_arf_ang_0"])
-
-print()
-
-fuselagem = contribuicao_fuselagem("fuselagem_2022")
-print("\33[92m---------- Contribuição da Fuselagem \33[m")
-print("Coeficiente Angular da Curva de Momento de Arfagem  : ", fuselagem["coef_ang_curva_momento_arf"])
-print("Coeficiente de momento de Arfagem para o ângulo de Ataque Nulo : ", fuselagem["coef_momento_arf_ang_0"])
-
-print()
-
-total = contribuicao_total(asa, emp_horizontal, fuselagem)
-print("\33[92m---------- Contribuição Total \33[m")
-print("Coeficiente Angular da Curva de Momento de Arfagem Total : ", total["coef_ang_curva_momento_arf"])
-print("Coeficiente de momento de Arfagem para o ângulo de Ataque Nulo Total: ", total["coef_momento_arf_ang_0"])
-
-print()
-
-# Quanto maior a Margem Estatística, maior será a estabilidade
-# 10% < Margem Estatística < 20%  ----  Boa Estabilidade e Manobrabilidade
-ponto_neutro = ponto_neutro(emp_horizontal, fuselagem, "asa_2022")
-margem_estatica = margem_estatica("asa_2022", ponto_neutro)
-print("\033[34m---------- Ponto Neutro e Margem Estática \033[0m")
-print("Ponto Neutro da Corda Média: %.2f%% " % ponto_neutro)
-print("Margem Estatística: %.2f%% " % margem_estatica)
-
-print()
-
-estabilidade = teste_estabilidade(total)
-print("\033[35m---------- Teste de Estabilidade:\033[0m", estabilidade)
-print()
+exibir_resultados(calcular_estabilidade("asa_2022", "empenagem_horizontal_2022", "fuselagem_2022", 0.25, 0.33))
